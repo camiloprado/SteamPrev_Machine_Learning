0a6ced4ba12d4cda2e314ab4485798532a9d2152
camiloprado <camilovgprado21@gmail.com>
2026-02-27 20:24:20 -0300

    feat: Implementa classe PostgreSQLBDGeral com métodos para criar tabela, buscar e inserir dados em bulk

---
+++ prj_TCC_PREVISOR_STEAM/classes/SQL/postgre_bdgeral.py
@@ -0,0 +1,160 @@
+from prj_TCC_PREVISOR_STEAM.classes.framework.AllSettings import Settings
+from prj_TCC_PREVISOR_STEAM.classes.SQL.postgre import PostgreSQL
+
+from psycopg2.extras import execute_batch, execute_values
+import json, logging
+
+logger = logging.getLogger(__name__)
+
+class PostgreSQLBDGeral(PostgreSQL):
+    """
+    Classe para operações com PostgreSQL.
+    """
+    
+    @classmethod
+    def criar_tabela_steam_geral(cls) -> None:
+        """
+        Cria a tabela steam_geral no banco de dados, se ainda não existir.
+        """
+        var_connConnection = cls.conectar()
+        
+        try:
+            var_strSQL = """
+            CREATE TABLE IF NOT EXISTS steam_geral (
+                appid INTEGER PRIMARY KEY,
+                id_itad VARCHAR(100) FOREIGN KEY REFERENCES steam_itad_mapping(id_itad) ON DELETE SET NULL,
+                nome VARCHAR(600) NOT NULL,
+                classificacao_etaria VARCHAR(50),
+                linguagens TEXT[],
+                desenvolvedores TEXT[],
+                distribuidores TEXT[],
+                preco VARCHAR(50),
+                metacritic_score VARCHAR(10),
+                categorias TEXT[],
+                genero TEXT[],
+                data_lancamento VARCHAR(50),
+                type VARCHAR(50),
+                review_score INTEGER,
+                total_reviews INTEGER,
+                total_negative INTEGER,
+                total_positive INTEGER,
+                review_score_desc VARCHAR(255),
+                historico_precos JSONB,
+                ultima_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            """
+            with var_connConnection.cursor() as cursor:
+                cursor.execute(var_strSQL)
+                var_connConnection.commit()
+                logger.info("Tabela steam_geral criada ou já existe.")
+        except Exception as e:
+            logger.error(f"Erro ao criar tabela steam_geral: {e}")
+            if var_connConnection:
+                var_connConnection.rollback()
+            raise Exception(f"Erro ao criar tabela steam_geral: {e}")
+        finally:
+            cls.desconectar(var_connConnection)
+
+    @classmethod
+    def buscar_dados_Geral(cls) -> list:
+        """
+        Busca dados na tabela steam_geral.
+
+        Retorna:
+        - list[dict]: Lista de dicionários com os dados dos jogos.
+        """
+        var_connConnection = cls.conectar()
+        
+        try:
+            var_strSQLGeral = """
+            SELECT *
+            FROM steam_unificado su
+            LEFT JOIN steam_itad_mapping sim ON su.appid = sim.appid
+            LEFT JOIN itad_raw ir ON sim.id_itad = ir.id_itad;
+            """
+
+            with var_connConnection.cursor() as var_curCursor:
+                var_curCursor.execute(var_strSQLGeral)
+                var_listResultados = var_curCursor.fetchall()
+                var_listColnames = [desc[0] for desc in var_curCursor.description]
+                var_listDados = [dict(zip(var_listColnames, row)) for row in var_listResultados]
+
+            logger.info(f"Dados buscados com sucesso da tabela steam_geral. Total de registros: {len(var_listDados)}")
+            return var_listDados
+
+        except Exception as err:
+            logger.error(f"Erro ao buscar dados steam_geral: {err}")
+            raise Exception(f"Erro ao buscar dados em steam_geral: {err}")
+        finally:
+            cls.desconectar(var_connConnection)
+
+    @classmethod
+    def inserir_dados_Geral_Bulk(cls, arg_listDados:list) -> None:
+        """
+        Busca e insere ou atualiza dados em bulk na tabela steam_geral.
+
+        Parâmetros:
+        - arg_listDados (list[dict]): Lista de dicionários com os dados dos jogos a serem inseridos ou atualizados.
+        """
+        var_connConnection = cls.conectar()
+        try:
+            var_strSQLInsert = """
+            INSERT INTO steam_geral (
+                appid, id_itad, nome, classificacao_etaria, linguagens, desenvolvedores, distribuidores, preco, metacritic_score, categorias, genero, data_lancamento, type, review_score, total_reviews, total_negative, total_positive, review_score_desc, historico_precos
+            ) VALUES %s
+            ON CONFLICT (appid) DO UPDATE SET
+                id_itad = EXCLUDED.id_itad,
+                nome = EXCLUDED.nome,
+                classificacao_etaria = EXCLUDED.classificacao_etaria,
+                linguagens = EXCLUDED.linguagens,
+                desenvolvedores = EXCLUDED.desenvolvedores,
+                distribuidores = EXCLUDED.distribuidores,
+                preco = EXCLUDED.preco,
+                metacritic_score = EXCLUDED.metacritic_score,
+                categorias = EXCLUDED.categorias,
+                genero = EXCLUDED.genero,
+                data_lancamento = EXCLUDED.data_lancamento,
+                type = EXCLUDED.type,
+                review_score = EXCLUDED.review_score,
+                total_reviews = EXCLUDED.total_reviews,
+                total_negative = EXCLUDED.total_negative,
+                total_positive = EXCLUDED.total_positive,
+                review_score_desc = EXCLUDED.review_score_desc,
+                historico_precos = EXCLUDED.historico_precos,
+                ultima_atualizacao = CURRENT_TIMESTAMP;
+            """
+
+            var_listValores = []
+            for var_dictDado in arg_listDados:
+                var_listValores.append((
+                    var_dictDado.get("appid"),
+                    var_dictDado.get("id_itad"),
+                    var_dictDado.get("nome"),
+                    var_dictDado.get("classificacao_etaria"),
+                    var_dictDado.get("linguagens"),
+                    var_dictDado.get("desenvolvedores"),
+                    var_dictDado.get("distribuidores"),
+                    var_dictDado.get("preco"),
+                    var_dictDado.get("metacritic_score"),
+                    var_dictDado.get("categorias"),
+                    var_dictDado.get("genero"),
+                    var_dictDado.get("data_lancamento"),
+                    var_dictDado.get("type"),
+                    var_dictDado.get("review_score"),
+                    var_dictDado.get("total_reviews"),
+                    var_dictDado.get("total_negative"),
+                    var_dictDado.get("total_positive"),
+                    var_dictDado.get("review_score_desc"),
+                    json.dumps(var_dictDado.get("historico_precos", []))
+                ))
+            with var_connConnection.cursor() as var_curCursor:
+                execute_values(var_curCursor, var_strSQLInsert, var_listValores)
+                var_connConnection.commit()
+
+        except Exception as err:
+            logger.error(f"Erro ao inserir dados steam_geral: {err}")
+            if var_connConnection:
+                var_connConnection.rollback()
+            raise Exception(f"Erro ao inserir dados em bulk em steam_geral: {err}")
+        finally:
+            cls.desconectar(var_connConnection)
